4ca956c838cd3d175ab2f93bd741e6b1a3adeadd
ccjmk <i.guastalli@gmail.com>
2020-12-10 12:59:30 +0000

    skills queried and added when creating actor

--- test.js
+++ test.js
@@ -1,54 +1,75 @@
 (async ()=> {
     // Get compendiums
     const raceNames = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Tiefling']; // race names are listed for filtering actual Races from Racial Features
-
     let _races = await game.packs.get("dnd5e.races").getContent();
     const races = _races.filter(r => raceNames.includes(r.data.name));
     const racialFeatures = _races.filter(r => !(raceNames.includes(r.data.name)));
     const classes = await game.packs.get("dnd5e.classes").getContent();
     const classFeatures = await game.packs.get("dnd5e.classfeatures").getContent();
 /*
- //   console.log('---------- RACES ---------------');
-   // races.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
-   // console.log('---------- RACIAL FEATURES ---------------');
-   // racialFeatures.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
- //   console.log('---------- CLASSES ---------------');
- //   classes.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
- //   console.log('---------- CLASS FEATURES ---------------');
-   // classFeatures.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
+//   console.log('---------- RACES ---------------');
+// races.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
+// console.log('---------- RACIAL FEATURES ---------------');
+// racialFeatures.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
+//   console.log('---------- CLASSES ---------------');
+//   classes.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
+//   console.log('---------- CLASS FEATURES ---------------');
+// classFeatures.forEach((c,i) => console.log(`${i}: ${c.data.name}`));
 */
+
     // Ask basic info
-    
-    const data = [[`What is your name? `, `text`], [`What is your race? `, `select`, raceNames], [`What is your class? `, `select`, Array.from(classes.map(c => c.data.name))]];
-    let [name, raceSelected, classSelected] = await multi_input({ title : `Summon Multi-roll!`, data });
-    
-    console.log(`Creating..
-    Name: ${name}
-    Race: ${raceSelected}
-    Class: ${classSelected}
-    `);
+    const classNames = Array.from(classes.map(c => c.data.name));
+    const basicInfo = [[`What is your name? `, `text`], [`What is your race? `, `select`, raceNames], [`What is your class? `, `select`, classNames], [`Dry-run? `, `checkbox`]];
+    let [name, raceSelected, classSelected, dryRun] = await multi_input({ title: `Choose name, race and class`, data: basicInfo });
 
     const raceItem = races.find(r => r.data.name === raceSelected);
     const classItem = classes.find(c => c.data.name === classSelected);
 
     console.log(raceItem)
     console.log(classItem)
+
+    // Select starting skills
+    const skillsToPick = await classes[0].data.data.skills.number;
+    const skillChoicesArray = await classItem.data.data.skills.choices;
+    const skillChoices = skillChoicesArray.map(s => `${game.dnd5e.config.skills[s]} ;checkbox`).map(s => s.split(';'));
+    console.log(`${name}'s ${classItem.data.name} class available skills (${skillsToPick}): `);
+    console.log(skillChoices.map(s => s[0]));
+    const selectedSkills = await multi_input({ title: `Choose class skills (${skillsToPick})`, data : skillChoices });
+
+
+    // Rolling Stats
     
-    // Create actor
-    let actor = await Actor.create({
-      name: name,
-      type: "character",
-      img: "icons/svg/mystery-man.svg",
-      folder: null,
-      sort: 12000,
-      data: {},
-      token: {},
-      items: [raceItem, classItem],
-      flags: {}
-    });
 
+    let actorData = {};
+    selectedSkills.forEach((e,i) => e ? (actorData[`skills.${skillChoicesArray[i]}.value`] = 1) : null);
+    
+    let actor;
+    if(!dryRun) {
+        // Create actor
+        console.log(`Creating..
+    Name: ${name}
+    Race: ${raceSelected}
+    Class: ${classSelected}`);
+        
+        actor = await Actor.create({
+        name: name,
+        type: "character",
+        img: "icons/svg/mystery-man.svg",
+        folder: null,
+        sort: 12000,
+        data: actorData,
+        token: {},
+        items: [raceItem, classItem],
+        flags: {}
+        });
+    } else {
+        console.log("> DRY-RUN: ");
+        console.log(`Name: ${name}`);
+        console.log(`Race: ${raceItem.data.name}`);
+        console.log(`Class: ${classItem.data.name}`);
+        console.log(`Skills: ${selectedSkills.map((s,i) => s ? skillChoicesArray[i] : false).filter(s => s)}`);
+    }
 
-    // Update race and class
     
 })();
 
@@ -63,6 +84,7 @@ async function multi_input({title = ``, data = []} = {})
     * If type is select, a 3rd value containing the list of options needs to be provided, 
     * which will be used both as value and as text for the select
     */
+
     let value = await new Promise((resolve)=> {
     new Dialog({
         title,       
@@ -71,12 +93,8 @@ async function multi_input({title = ``, data = []} = {})
             label : `Ok`, 
             callback : (html) => {
                 let selects = html.find("select");
-                console.log(selects);
                 let html_values = html.find("input");
-                console.log(html_values)
                 resolve(data.map((e,i) => {
-                    console.log("e")
-                    console.log(e)
                     switch(e[1]) {
                         case "number":
                             return html_values[i].valueAsNumber;
